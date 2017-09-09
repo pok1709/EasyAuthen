4d43f317a849d69eecd3a34f5430866b3e4c2187
pok1709 <pok1709@gmail.com>
2017-09-09 15:54:17 +0700

    Done

--- EasyAuthen/ViewController.swift
+++ EasyAuthen/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let loginDictionary = ["user1" : "pass1",
+                           "user2" : "pass2"]
+    
+    var userString = ""
+    var passwordString = ""
+    
+    let alertArray = ["User Fails", "Password Fail", "Welcome User"]
+    
+    
+    
+    @IBOutlet weak var messageLabelOutlet: UILabel!
+    
+    @IBOutlet weak var userTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var passwordTextFieldOutlet: UITextField!
+    
+    @IBAction func loginButtonOutlet(_ sender: Any) {
+        
+        userString = userTextFieldOutlet.text!
+        passwordString = passwordTextFieldOutlet.text!
+        
+        print("user : \(userString)")
+        print("password : \(passwordString)")
+        
+        let resultString = loginDictionary[userString]
+        
+        print("result : \(String(describing: resultString))")
+        
+        if (resultString == nil) {
+            messageLabelOutlet.text = alertArray[0]
+        } else {
+            if(passwordString != resultString){
+                messageLabelOutlet.text = alertArray[1]
+            }else{
+                messageLabelOutlet.text = alertArray[2]
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
